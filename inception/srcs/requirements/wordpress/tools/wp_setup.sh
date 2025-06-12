043f2c7fe5db54a2659c50c3db8dcff99323c687
BishopVK <danielgraindorge@gmail.com>
2025-06-13 00:51:26 +0200

    Fixed wrong typo path

--- inception/srcs/requirements/wordpress/tools/wp_setup.sh
+++ inception/srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Descargar WP-CLI
-cd /ust/local/bin
+cd /usr/local/bin
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar wp
