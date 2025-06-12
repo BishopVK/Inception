eecbc817e7e4c39b49057ef8a74944b4b1ec4b57
BishopVK <danielgraindorge@gmail.com>
2025-06-13 01:45:08 +0200

    Updated sh

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Sustituye variables antes de lanzar MariaDB
-/usr/local/bin/substitute_env_vars.sh
+#/usr/local/bin/substitute_env_vars.sh
 
-# Inicializa la base de datos si no existe (primera vez)
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-  echo "⚙️ Inicializando base de datos..."
-
-  mysqld --user=mysql --bootstrap < /etc/mysql/init.sql
+# Solo inicializa si no existe ya la base de datos
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "⚙️ Inicializando base de datos..."
+    mysql_install_db --user=mysql > /dev/null
+    mysqld --user=mysql --bootstrap < /etc/mysql/init.sql
 fi
 
 # Inicia MariaDB en primer plano
-exec mysqld
+exec mysqld --user=mysql
 
 #mysql_install_db
 #mysqld
