f1be6784ba970734db507d0d9d732e46dc7a8af2
BishopVK <danielgraindorge@gmail.com>
2025-06-12 23:35:56 +0200

    Fixed mariadb dockerfile

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-mysql_install_db
-mysqld
+# Sustituye variables antes de lanzar MariaDB
+/usr/local/bin/substitute_env_vars.sh
+
+# Inicia MariaDB en primer plano
+exec mysqld
+
+#mysql_install_db
+#mysqld
