0f9bac26bafccd937689e5ea1171ac6c708e82bb
BishopVK <danielgraindorge@gmail.com>
2025-06-13 00:47:13 +0200

    Fixed name of file wp-cli.phar

--- inception/srcs/requirements/wordpress/tools/wp_setup.sh
+++ inception/srcs/requirements/wordpress/tools/wp_setup.sh
@@ -4,7 +4,7 @@
 cd /ust/local/bin
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp.cli.phar wp
+mv wp-cli.phar wp
 
 # Preparar el directorio web
 mkdir -p /var/www/html
