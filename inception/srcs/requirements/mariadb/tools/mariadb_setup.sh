a4324069577ed16ba343e9e11bd9a1cbf46acb6c
BishopVK <danielgraindorge@gmail.com>
2025-06-13 01:36:09 +0200

    Fixed script to execute init.sql

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -3,6 +3,13 @@
 # Sustituye variables antes de lanzar MariaDB
 /usr/local/bin/substitute_env_vars.sh
 
+# Inicializa la base de datos si no existe (primera vez)
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+  echo "⚙️ Inicializando base de datos..."
+
+  mysqld --user=mysql --bootstrap < /etc/mysql/init.sql
+fi
+
 # Inicia MariaDB en primer plano
 exec mysqld
 
